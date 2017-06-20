a87afabc6f4ba61b59000e08d9ecb7105048851e
harsh.nitmas <harsh.nitmas@gmail.com>
2017-06-20 18:24:58 +0530

    Login Page

---
+++ Online Examination System/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Examination_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OnlineExaminationDBEntities db = new OnlineExaminationDBEntities())
+                {
+                    var checkUser = db.Users.Where(a => a.UserName.Equals(u.UserName) && a.Password.Equals(u.Password)).FirstOrDefault();
+                    if (checkUser != null)
+                    {
+                        Session["UserId"] = checkUser.UserId;
+                        Session["UserName"] = checkUser.UserName;
+                        if (checkUser.UserType == "Admin")
+                        {
+                            return RedirectToAction("AdminHomePage");
+                        }
+                        if (checkUser.UserType == "User")
+                        {
+                            return RedirectToAction("UserHomePage");
+                        }
+                        
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "The user name or password is incorrect");
+                        return View(u);
+                    }
+                   
+                }
+            }
+           
+            return View(u);
+        }
+        public ActionResult AdminHomePage()
+        {
+            if (Session["UserId"] != null)
+                return View();
+            else
+                return RedirectToAction("Login");
+        }
+        public ActionResult UserHomePage()
+        {
+            if (Session["UserId"] != null)
+                return View();
+            else
+                return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Online Examination System/Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Examination_System.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterUser(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OnlineExaminationDBEntities db = new OnlineExaminationDBEntities())
+                {
+                    u.UserType="User";
+                    db.Users.Add(u);
+                    var res=db.SaveChanges();
+
+                    if (res != 0)
+                    {
+                        ViewData["Result"] = "Success";
+                        return View();
+                    }
+                    else
+                    {
+                         ViewData["Result"] = "Failed";
+                         return View();
+
+                    }
+                }
+            }
+            return View(u);
+            
+        }
+    }
+}
