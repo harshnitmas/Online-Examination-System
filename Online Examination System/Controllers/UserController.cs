d9a8334658d4f314181b7564a5b7e98480d39f24
harsh.nitmas <harsh.nitmas@gmail.com>
2017-06-21 19:07:29 +0530

    Manage User Portal-Css Changes

---
+++ Online Examination System/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Examination_System.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult UpdateUserDetails()
+        { 
+            return View();
+        }
+    }
+}
